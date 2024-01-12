9c27c3418d15196d7d399cb8db8728dd963fbfb9
NisovaKi <oksy169516@gmail.com>
2024-01-12 22:43:46 +0300

    Changed header.jsx

--- knoff/src/views/Header/Header.jsx
+++ knoff/src/views/Header/Header.jsx
@@ -1,12 +1,24 @@
 /* eslint-disable prettier/prettier */
-
-import { Container } from "../Container/Container";
-
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable prettier/prettier */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
 /* eslint-disable semi */
+import { Container } from "../Container/Container.jsx";
+import s from './Header.module.scss';
+
 export const Header = () => (
   <header className="{s.header}">
     <Container>
-      <p>Hello</p>
+      <div className="{s.logo}">
+        <Logo />
+      </div>
+      <div className="{s.search}">
+        <SearchForm />
+      </div>
+      <div className="{s.navigation}">
+        <Navigation />
+      </div>
     </Container>
   </header>
-)
+);
