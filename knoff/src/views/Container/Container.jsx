15e32c411c848cd6b9d2870119be3eb4cc72077d
NisovaKi <oksy169516@gmail.com>
2024-01-12 21:28:10 +0300

    the code is entered into the files container, Footer, header, main

---
+++ knoff/src/views/Container/Container.jsx
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+export const Container = (props) => <div>{props.children}</div>;
---
+++ knoff/src/views/Main/Main.jsx
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+export const Main = () => <main></main>;
